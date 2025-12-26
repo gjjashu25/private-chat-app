9182a3204329991ea729e76b73c37efc0cf53089
gjjashu25 <gjjashu25@gmail.com>
2025-12-26 11:47:14 +0530

    Create script.js

---
+++ static/script.js
@@ -0,0 +1,44 @@
+const socket = io();
+let room = "";
+let username = "";
+
+function joinChat() {
+    username = document.getElementById("username").value;
+    room = document.getElementById("room").value;
+
+    if (!username || !room) return;
+
+    socket.emit("join", { username, room });
+
+    document.querySelector(".join-box").classList.add("hidden");
+    document.querySelector(".chat-box").classList.remove("hidden");
+}
+
+socket.on("message", function(msg) {
+    const div = document.createElement("div");
+
+    if (msg.startsWith(username + ":")) {
+        div.className = "my-message";
+    } else {
+        div.className = "other-message";
+    }
+
+    div.innerText = msg;
+    document.getElementById("messages").appendChild(div);
+    document.getElementById("messages").scrollTop =
+        document.getElementById("messages").scrollHeight;
+});
+
+function sendMessage() {
+    const message = document.getElementById("msg").value;
+    if (message.trim() === "") return;
+
+    socket.emit("message", { username, room, message });
+    document.getElementById("msg").value = "";
+}
+
+document.getElementById("msg").addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+        sendMessage();
+    }
+});
